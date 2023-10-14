c7cce03ec4ecc8fff6c9550f27099126de989d00
Ggjorven <Jorbenvanderwal@gmail.com>
2023-10-14 15:48:39 +0200

    Fix: removed GLEW as a submodule

---
+++ vendor/GLEW/premake5.lua
@@ -0,0 +1,48 @@
+project "GLEW"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/glew.c",
+		"src/glewinfo.c",
+		"src/visualinfo.c",
+		"auto/src/glew_head.c",
+		"auto/src/glewinfo_head.c"
+
+	}
+
+	includedirs
+	{
+		"auto",
+		"include",
+		"src"
+	}
+
+	defines
+	{
+		"_CRT_NO_SECURE_WARNINGS",
+		"GLEW_STATIC"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
